4e1f2b3642aca866104ccb35b0943c2e623df68a
unitehenry <unitehenry@gmail.com>
2019-10-19 18:33:34 -0400

    manifest

--- app/src/AskQuestion.js
+++ app/src/AskQuestion.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AskQuestion.css';
 
+const Choice = ({ choice }) => {
+    return (
+        <div key={`${i}${choice}`} className="option">
+            <p>{choice}</p>
+            <span>X</span>
+        </div>
+    )
+}
+
 const AskQuestion = () => {
+    const [question, setQuestion] = useState('');
+    const [choices, setChoices] = useState([]);
+
+    const addChoice = (e) => {
+        if (e.keyCode === 13) {
+            const newchoices = choices;
+            newchoices.push(e.target.value);
+            setChoices(newchoices);
+            e.target.value = '';
+        }
+    }
+
     return (
         <div className="AskQuestion">
             <div className="question-card">
                 <input type="text" placeholder="ask a question" />
                 <div className="choice">
-                    <input type="text" placeholder="add a choice" />
+                    <input type="text" placeholder="add a choice" onKeyUp={e => addChoice(e)} />
                 </div>
                 <div className="options">
-                    <div className="option">
-                        <p>option</p>
-                        <span>X</span>
-                    </div>
+                    {
+                        choices.map((choice, i) => <Choice choice={choice} />)
+                    }
                 </div>
                 <button className="submit-btn">submit</button>
                 <p className="footnote">
