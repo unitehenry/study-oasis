acad5f40dc930546d3e4f76f760d2e9e63ad9d08
David Kim <d@vid.kim>
2019-10-19 13:40:53 -0400

    Test blockstacks
    test auth

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import Profile from './Profile.js';
+import Signin from './SignIn.js';
+import { UserSession } from 'blockstack';
+import { appConfig } from './assets/constants'
+
+
 import './App.css';
 
 // Components
-import SignIn from './SignIn';
+// import SignIn from './Signin';
 
-function App() {
-  return (
-    <div className="App">
-      <SignIn />
-    </div>
-  );
+const userSession = new UserSession({ appConfig })
+
+
+export default class App extends Component {
+
+  handleSignIn(e) {
+    e.preventDefault();
+    userSession.redirectToSignIn();
+  }
+
+  handleSignOut(e) {
+    e.preventDefault();
+    userSession.signUserOut(window.location.origin);
+  }
+
+  render() {
+    return (
+      <div className="site-wrapper">
+        <div className="site-wrapper-inner">
+          { !userSession.isUserSignedIn() ?
+            <Signin userSession={userSession} handleSignIn={ this.handleSignIn } />
+            : <Profile userSession={userSession} handleSignOut={ this.handleSignOut } />
+          }
+        </div>
+      </div>
+    );
+  }
+
+  componentWillMount() {
+    if (userSession.isSignInPending()) {
+      userSession.handlePendingSignIn().then((userData) => {
+        //if (!userData.username) {
+        //  throw new Error('This app requires a username.')
+        //}
+        window.location = window.location.origin;
+      });
+    }
+  }
+
 }
-
-export default App;
