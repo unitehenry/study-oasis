c7a9f7af4dd74605157441ab6d9a3f2a051b2d1f
unitehenry <unitehenry@gmail.com>
2019-10-19 20:21:28 -0400

    submit question

--- app/src/AskQuestion.js
+++ app/src/AskQuestion.js
@@ -11,7 +11,7 @@ const Choice = ({ choice, removeChoice }) => {
 }
 
 const AskQuestion = () => {
-    // const [question, setQuestion] = useState('');
+    const [question, setQuestion] = useState('');
     const [choices, setChoices] = useState([]);
 
     const addChoice = (e) => {
@@ -25,17 +25,22 @@ const AskQuestion = () => {
         setChoices(choices.filter((c, index) => index !== i));
     }
 
+    const submitQuestion = () => {
+        // submit question
+        console.log({ question, choices });
+    }
+
     return (
         <div className="AskQuestion">
             <div className="question-card">
-                <input type="text" placeholder="ask a question" />
+                <input type="text" placeholder="ask a question" onChange={e => setQuestion(e.target.value)}/>
                 <div className="choice">
                     <input type="text" placeholder="add a choice" onKeyUp={e => addChoice(e)} />
                 </div>
                 <div className="options">
                     { choices.map((choice, i) => <Choice key={`${i}${choice}`} choice={choice} removeChoice={() => removeChoice(i)}/> ) }
                 </div>
-                <button className="submit-btn">submit</button>
+                <button className="submit-btn" onClick={submitQuestion}>submit</button>
                 <p className="footnote">
                     <i>just submit if non multiple-choice question</i>
                 </p>
